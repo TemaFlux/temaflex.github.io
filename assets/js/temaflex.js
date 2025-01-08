28223d1fbaaeb71b666a7d582d8c1bb0e4027f0d
TemaFlux <temaflex1337@gmail.com>
2025-01-08 12:53:22 +0200

    Upload to latest changes

--- assets/js/temaflex.js
+++ assets/js/temaflex.js
@@ -1,158 +1,99 @@
-//OSNOVNOE BEZ NEGO NE KAK A TI IDI V PEN
-//by TemaFlux.tk
 function muteCheck() {
 	new YouTubeToHtml5({
 		attribute: 'youtube',
 		selector: 'video[youtube]',
 		withAudio: true // Filter streams to those with audio channels
 	});
-    if ($["session"]["get"]("mute") !== "false") {
-		if (typeof audio != "undefined")
-			audio["volume"] = 0;
-		if (typeof video != "undefined")
-			video["volume"] = 0;
-    } else {
-		awvolume(false);
-        // $("pre")["removeClass"]("fa-volume-off")["addClass"]("fa-volume-up")
-    }
+
+	mute(false);
 }
-function mute() {
-	/*if (typeof audio != "undefined") {
-		if (audio["volume"] >= 0.9) {
-			awvolume(true);
-			// $("pre")["removeClass"]("fa-volume-up")["addClass"]("fa-volume-off");
-			for (var sound = 0.9; sound > 0; sound = sound - 0.1) {
-				sleep(39);
-				console.log("Volume: " + sound);
-				audio["volume"] = sound;
-			};
-			$["session"]["set"]("mute", "true")
-		} else {
-			awvolume(false);
-			// $("pre")["removeClass"]("fa-volume-off")["addClass"]("fa-volume-up");
-			for (var sound = 0; sound < 1; sound = sound + 0.1) {
-				console.log("Volume: " + sound);
-				audio["volume"] = sound;
-				sleep(39);
-			};
-			$["session"]["set"]("mute", "false")
-		};
-	}*/
+
+async function mute(smoothly = true) {
+	await changeVolume(video.volume > 0 ? 0 : 0.2, smoothly);
+}
+
+async function changeVolume(value, smoothly = true) {
 	if (typeof video != "undefined") {
-		if (video["volume"] >= 0.9) {
-			awvolume(true);
-			// $("pre")["removeClass"]("fa-volume-up")["addClass"]("fa-volume-off");
-			for (var sound = 0.9; sound > 0; sound = sound - 0.1) {
-				sleep(39);
-				video["volume"] = sound;
-			};
-			$["session"]["set"]("mute", "true")
-		} else {
-			awvolume(false);
-			// $("pre")["removeClass"]("fa-volume-off")["addClass"]("fa-volume-up");
-			for (var sound = 0; sound < 1; sound = sound + 0.1) {
-				video["volume"] = sound;
-				sleep(39);
-			};
-			$["session"]["set"]("mute", "false")
-		};
+		var currentlyVolume = video.volume;
+		var isMute = currentlyVolume > value;
+		var classList = document.getElementById("mute").classList;
+
+		if (smoothly) {
+			if (isMute) {
+				while (typeof video != "undefined" && video.volume != value) {
+					await sleep(50);
+					var nVolume = toFixed(video.volume - 0.01);
+					var precent = (video.volume = nVolume > value ? nVolume : value) / currentlyVolume;
+
+					if (precent <= 0) {
+						classList.remove("fa-volume-down");
+						classList.add("fa-volume-off");
+					} else if (precent < 0.5) {
+						classList.remove("fa-volume-up");
+						classList.add("fa-volume-down");
+					}
+				}
+			} else {
+				while (typeof video != "undefined" && video.volume != value) {
+					await sleep(50);
+					var nVolume = toFixed(video.volume + 0.01);
+					var precent = (video.volume = nVolume > value ? value : nVolume) / value;
+
+					if (precent > 0.5) {
+						classList.remove("fa-volume-down");
+						classList.add("fa-volume-up");
+					} else if (precent > 0) {
+						classList.remove("fa-volume-off");
+						classList.add("fa-volume-down");
+					}
+				}
+			}
+		} else video.volume = value;
+
+		classList.remove("fa-volume-" + (isMute ? "up" : "down"));
+		classList.add("fa-volume-" + (isMute ? "down" : "up"));
 	}
-    delete sound
 }
 
-function sleep(time) {
-    time += new Date()["getTime"]();
-    while (new Date() < time) {}
-}
+function toFixed(value) { return value === +value && value !== (value|0) ? value : parseFloat(value.toFixed(1)); }
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
-function awvolume(mute = false) {
-	if (mute) {
-		document.getElementById("mute").classList.remove("fa-volume-up");
-		document.getElementById("mute").classList.add("fa-volume-off");
-	}
-	else {
-		document.getElementById("mute").classList.remove("fa-volume-off");
-		document.getElementById("mute").classList.add("fa-volume-up");
-	}
-}
-
-function rand(muted = false, rand = 0, opacity = false) {
+function rand(opacity = false) {
     if (statusBut) {
-        if (muted == false) {
-            $["session"]["set"]("mute", "false");
-			awvolume(false);
-            // $("pre")["removeClass"]("fa-volume-off")["addClass"]("fa-volume-up")
-		}
-		else {
-			$["session"]["set"]("mute", "true");
-			awvolume(true);
-			// $("pre")["removeClass"]("fa-volume-up")["addClass"]("fa-volume-off");
-		}
-        scrollOne["scrollAmount"] = 500;
-        scrollTwo["scrollAmount"] = 500;
-        /*if (rand == 0)
-            rand = randCashe
-        if (opacity)
-            rand = video["src"]["split"]("y/")[1]["split"](".")[0]
-		$["session"]["set"]("id", rand);
-		
-        var src_video = "https://google.temaflex.tk/beauty/" + rand + ".mp4";
-        var src_audio = "https://google.temaflex.tk/beauty/" + rand + ".mp3";
-		video["src"] = src_video;
-		if (typeof audio != "undefined") {
-			for (var sound = 0.9; sound > 0; sound = sound - 0.1) {
-				sleep(39);
-				audio["volume"] = sound
-			};
-			
-			audio["volume"] = 1;
-			audio["src"] = src_audio;
-		}*/
+      scrollOne["scrollAmount"] = 500;
+      scrollTwo["scrollAmount"] = 500;
 
-		httpGetAsync("/beauty.php?r="+document.getElementById("video")["youtube"], function(url) {
-			if (typeof video != "undefined") {
-				for (var sound = 0.9; sound > 0; sound = sound - 0.1) {
-					sleep(39);
-					video["volume"] = sound;
+			httpGetAsync("/beauty.php?r="+document.getElementById("video")["youtube"], function(url) {
+				video["src"] = "";
+				video["youtube"] = url;
+				document.getElementById("video").setAttribute("src", "");
+				document.getElementById("video").setAttribute("youtube", url);
+				new YouTubeToHtml5({
+					attribute: 'youtube',
+					selector: 'video[youtube]',
+					withAudio: true // Filter streams to those with audio channels
+				});
+
+				video["oncanplay"] = function() {
+					if (typeof audio != "undefined") audio["play"]();
+					scrollOne["scrollAmount"] = 100;
+					scrollTwo["scrollAmount"] = 100;
 				};
-			}
-			video["src"] = "";
-			video["youtube"] = url;
-			document.getElementById("video").setAttribute("src", "");
-			document.getElementById("video").setAttribute("youtube", url);
-			new YouTubeToHtml5({
-				attribute: 'youtube',
-				selector: 'video[youtube]',
-				withAudio: true // Filter streams to those with audio channels
+
+				if (!opacity) {
+					document["getElementById"]("rand")["style"]["opacity"] = 0.1;
+					document["getElementById"]("rand")["style"]["cursor"] = "no-drop";
+					statusBut = false;
+
+					setTimeout(function() {
+						document["getElementById"]("rand")["style"]["cursor"] = "";
+						document["getElementById"]("rand")["style"]["opacity"] = 1;
+						statusBut = true
+					}, 3000)
+				}
 			});
-			video["volume"] = 1;
-
-			video["oncanplay"] = function() {
-				if (typeof audio != "undefined") audio["play"]();
-				scrollOne["scrollAmount"] = 100;
-				scrollTwo["scrollAmount"] = 100;
-				/*randCashe = randomInt(1, max, rand);
-				preLoadVideo["src"] = "https://google.temaflex.tk/beauty/" + randCashe + ".mp4";
-				preLoadVideo["oncanplay"] = function() {
-					stop()
-				};
-				preLoadAudio["src"] = "https://google.temaflex.tk/beauty/" + randCashe + ".mp3";*/
-			};
-	
-			if (!opacity) {
-				document["getElementById"]("rand")["style"]["opacity"] = 0.1;
-				document["getElementById"]("rand")["style"]["cursor"] = "no-drop";
-				statusBut = false;
-				setTimeout(function() {
-					document["getElementById"]("rand")["style"]["cursor"] = "";
-					document["getElementById"]("rand")["style"]["opacity"] = 1;
-					statusBut = true
-				}, 3000)
-			}
-		});
     }
-}
-statusBut = true;
+} statusBut = true;
 
 function randomInt(min, max, n_repeat = rand) {
     var number;
@@ -161,8 +102,7 @@ function randomInt(min, max, n_repeat = rand) {
             return number;
         }
     }
-}
-randCashe = randomInt(1, max)
+} randCashe = randomInt(1, max)
 
 function clock() {
 	//Save the times in variables
@@ -186,20 +126,15 @@ function clock() {
 	else minutes = minutes;
 	if (seconds < 10) seconds = "0" + seconds;
 	else seconds = seconds;
-	if (document.getElementById("clock") != null)
-		document.getElementById("clock").innerHTML = (meridiem  + " " + hours + ":" + minutes);
-	if (document.getElementById("date") != null)
-		document.getElementById("date").innerHTML = (monthNames[month] + " " + day + " " + year);
-}
-setInterval('clock()', 1);
+	if (document.getElementById("clock") != null)	document.getElementById("clock").innerHTML = (meridiem  + " " + hours + ":" + minutes);
+	if (document.getElementById("date") != null) document.getElementById("date").innerHTML = (monthNames[month] + " " + day + " " + year);
+} setInterval('clock()', 1);
 
-function httpGetAsync(theUrl, callback)
-{
+function httpGetAsync(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp.responseText);
     }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
     xmlHttp.send(null);
 }
